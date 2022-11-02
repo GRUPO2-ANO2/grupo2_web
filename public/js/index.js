e60f7c215a945ed43aa091390bff476b68d84ed7
lipeeeee <filipe.pereirafga@gmail.com>
2022-11-02 15:20:49 +0000

    Firebase login beggining

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+
+import { 
+  hideLoginError, 
+  showLoginState, 
+  showLoginForm, 
+  showApp, 
+  showLoginError, 
+  btnLogin,
+  btnSignup,
+  btnLogout
+} from './ui';
+
+import { 
+  getAuth,
+  onAuthStateChanged, 
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth';
 
 // Web app's Firebase configuration
 const firebaseConfig = initializeApp({
@@ -14,4 +32,60 @@ const firebaseConfig = initializeApp({
 })
  
 // Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//#region auth
+// Login using email/password
+const loginEmailPassword = async () => {
+  console.log("debug")
+  const loginEmail = txtEmail.value
+  const loginPassword = txtPassword.value
+
+  await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+}
+  
+// Create new account using email/password
+const createAccount = async () => {
+  const email = txtEmail.value
+  const password = txtPassword.value
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password)
+  }
+  catch(error) {
+    console.log(`There was an error: ${error}`)
+    showLoginError(error)
+  } 
+}
+
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+      showApp()
+      showLoginState(user)
+
+      hideLoginError()
+      hideLinkError()
+    }
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `You're not logged in.`
+    }
+  })
+}
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
+btnLogin.addEventListener("click", loginEmailPassword) 
+btnSignup.addEventListener("click", createAccount)
+btnLogout.addEventListener("click", logout)
+
+const auth = getAuth(firebaseApp);
+
+monitorAuthState();
+//#endregion
