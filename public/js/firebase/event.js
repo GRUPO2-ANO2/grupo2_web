5629d6e3f42f15f8b0246dd3ce0edf4f7e73dd54
GONCAVF <100807310+GONCAVF@users.noreply.github.com>
2022-12-26 23:40:41 +0000

    showEvents

--- public/js/firebase/event.js
+++ public/js/firebase/event.js
@@ -91,19 +91,46 @@ async function showEvents(events){
     var location = "";
     var timeStamp = 0;
     var dateFormat = 0;
-    //var dateFinish = "";
-    for (var i = 0; i < events.length; i++){
-        location = events[i].location;
-        dateStart = events[i].dateStart;
-        dateFinish = events[i].dateFinish;
+    
+    // Get the card container element
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.classList.add('row gx-4 gx-lg-5 row-cols-2 justify-content-center');
+        
+    // Iterate through the data and create a card for each item
+    for (let i = 0; i < events.length; i++) {
 
-        timeStamp = events[i].dateStart;
-        dateFormat = new Date(timeStamp);
+        const div = document.createElement('div');
+        div.className = `container px-4 px-lg-5 mt-5`
 
-        document.getElementById("location").innerHTML = events[i].location;
-        document.getElementById("description").innerHTML = dateFormat;
-        //document.getElementById("description").innerHTML = events[i].dateFinish;
+        const div2 = document.createElement('div');
+        div2.className = `row gx-4 gx-lg-5 row-cols-2 justify-content-center`
+
+        const div3 = document.createElement('div');
+        div3.className = `col mx-5`
+
+        const div4 = document.createElement('div');
+        div4.className = `card h-100`
+
+        const div5 = document.createElement('div');
+        div5.className = `card-body p-4`
+
+        const div6 = document.createElement('div');
+        div6.className = `text-center`
+
+        const card = document.createElement('div');
+        card.id = `card-${i}`;
+        card.className = `fw-bolder`
+        card.innerHTML = `
+        <h5>${events[i].location}</h2>
+        `;
+
+
+        /*div6.appendChild(card);
+        div5.appendChild(div6);*/
+        div6.appendChild(card);
+        div5.appendChild(div6);
+        div4.appendChild(div5);
+        div3.appendChild(div4);
+        document.body.appendChild(div3);
     }
-
-    return event;
 }
