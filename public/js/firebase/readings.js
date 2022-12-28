b95c8e642fd354cf7e210a42cb5c7b54786de9e6
lipeeeee <filipe.pereirafga@gmail.com>
2022-12-28 15:03:27 +0000

    getGuiasDoneReadings

--- public/js/firebase/readings.js
+++ public/js/firebase/readings.js
@@ -15,3 +15,28 @@ async function getUserReadings(){
     
     return readings;
 }
+
+// Get all readings done by a guia
+async function getGuiasDoneReadings(){
+    var isGuia = await userIsGuia();
+    var readings = [];
+    var readingsCount = 0;   
+
+    // this check might be redundant if frontend done correctly
+    if (isGuia){
+        await firebase.firestore().collection("leituras").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                var data = doc.data();
+    
+                if (data.idGuia == currentUser.uid){
+                    readings[readingsCount] = data;
+                    readingsCount++;
+                }
+            });
+        });
+    } else {
+        console.log("user isnt guia");
+    }
+
+    return readings;
+}
