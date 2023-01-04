f4815d652f70b2696a8d98c10b2fc1d75646197c
GONCAVF <100807310+GONCAVF@users.noreply.github.com>
2023-01-04 12:15:34 +0000

    perfilEvents

--- public/js/firebase/event.js
+++ public/js/firebase/event.js
@@ -76,11 +76,11 @@ async function getEventsByUser(){
             if (docIdUtilizador == currentUser.uid){
                 enrolled = await userIsEnrolledInEvent(docIdEvento);
                 // And if enrolled, add event
-                if(enrolled){
+                if(enrolled){   
                     await firebase.firestore().collection("eventos").doc(docIdEvento).get().then((docE) =>{
                         if (docE.exists){
                             events[eventCount] = docE.data();
-                            eventCount++;
+                            eventCount++;                                                    
                         }
                     });
                 }
@@ -88,6 +88,107 @@ async function getEventsByUser(){
         });
     });
     return events;
+}
+
+async function showEventsByUser() {
+    var events = await getEventsByUser();
+
+    // Get the card container element
+    const cardContainer = document.getElementById('card-container');
+
+    // Create a row element
+    const row = document.createElement('div');
+    row.className = 'row';
+
+    // Create a card for each item
+    for (let i = 0; i < events.length; i++) {
+
+        if (events[i]) {
+            console.log(events[i].uid);
+          }
+
+      // Create a column
+      const col = document.createElement('div');
+      col.className = 'col-4 mt-3';
+
+      // Create the card
+      const card = document.createElement('div');
+      card.id = `card-${i}`;
+      card.className = `fw-bolder`;
+      card.innerHTML = `
+        <div class="card h-100">
+          <img class="card-img-top" src="${events[i].image}" alt="..." />
+          <div class="card-img-overlay">
+            <div class="text-center text-white">
+              <h5>${events[i].location}</h2>
+            </div>
+          </div>
+        </div>
+        `;
+
+      // Append the card to the column
+      col.appendChild(card);
+
+      // Append the column to the row
+      row.appendChild(col);
+
+      const modalContainer = document.getElementById('modal-container');
+
+      // Create the modal element
+      const modal = document.createElement('div');
+        modal.className = `modal-content`;
+        modal.innerHTML = `
+        <div class="card">
+          <div class="modal" id="modal-${events[i].uid}" tabindex="-1" aria-labelledby="modal-${events[i].uid}" aria-hidden="true">
+            <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+              <div class="modal-content bg-dark text-white">
+                <div class="modal-header border-0">
+                  <button class="btn-close" type="button" onclick="hideModal('modal-${events[i].uid}');" aria-label="Close"></button>
+                </div>
+                <div class="modal-body pb-5">
+                  <div class="row justify-content-center text-center">
+                    <div class="col-lg-8">
+                      <h5 class="modal-title text-secondary text-uppercase mb-0 text-center">${events[i].location}</h5>
+                      <img class="img-fluid rounded mb-5" style="margin-top: 20px;" src="${events[i].image}" />
+                      <div class="row">
+                        <div class="col-md-4">
+                          <h5>Localização</h5>
+                          <p>${events[i].location}</p>
+                          <h5>Data Inicio</h5>
+                          <p>${events[i].dateStart}</p>
+                          <h5>Data Fim</h5>
+                          <p>${events[i].dateFinish}</p>
+                        </div>
+                        <div class="col-md-8 ms-auto">
+                          <h5>Descrição</h5>
+                          <p>${events[i].description}</p>
+                        </div>
+                      </div>
+                      <div class="row d-grid gap-2 col-6 mx-auto">
+                        <button class="btn btn-sm btn-success" style="margin-top: 20px;" onclick="joinEvent('${events[i].uid}')">
+                          <i class="fas fa-xmark fa-fw"></i> Entrar </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;      
+
+        // Append the modal to the document body
+        modalContainer.appendChild(modal);
+
+
+        // Add an event listener to the card that opens the modal when clicked
+        card.addEventListener('click', function() {
+            var modalElement = document.getElementById(`modal-${events[i].uid}`);
+            modalElement.style.display = 'block';
+        });
+    }
+
+    // Append the row to the card container
+    cardContainer.appendChild(row);
 }
 
 // Return open events
