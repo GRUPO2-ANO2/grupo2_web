cda8b1dceca0cd0c380cb8dbd50d76c0acf0f04a
lipeeeee <filipe.pereirafga@gmail.com>
2023-01-30 23:59:37 +0000

    limit cpu firebase query with collection limits

--- public/js/firebase/api.js
+++ public/js/firebase/api.js
@@ -14,12 +14,12 @@ async function getApiDocById(id) {
     });
 }
 
-async function searchApiDocsByName(searchString) {
+async function searchApiDocsByName(searchString, limit = 10) {
     return new Promise((resolve) => {
         var ref = firebase.firestore().collection("api");
 
-        const query1 = ref.orderBy("name").startAt(searchString).endAt(searchString + "\uf8ff");
-        const query2 = ref.orderBy("alternateNames").startAt(searchString).endAt(searchString + "\uf8ff");
+        const query1 = ref.orderBy("name").startAt(searchString).endAt(searchString + "\uf8ff").limit(limit);
+        const query2 = ref.orderBy("alternateNames").startAt(searchString).endAt(searchString + "\uf8ff").limit(limit);
         const allResults = [];
 
         query1.get().then(querySnapshot1 => {
@@ -40,11 +40,10 @@ async function searchApiDocsByName(searchString) {
     })
 }
 
-
-async function searchApiDocsByCountry(countryCode) {
+async function searchApiDocsByCountry(countryCode, limit = 10) {
     return new Promise((resolve) => {
         var ref = firebase.firestore().collection("api");
-        const query = ref.where("country_code", "==", countryCode);
+        const query = ref.where("country_code", "==", countryCode).limit(limit);
         const allResults = [];
 
         query.get().then(function (querySnapshot) {
