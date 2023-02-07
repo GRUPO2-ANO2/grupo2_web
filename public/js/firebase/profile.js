02b3517dcd377215d4601e4a4702f845404c89e5
lipeeeee <filipe.pereirafga@gmail.com>
2023-02-07 09:35:18 +0000

    graph profile guia

--- public/js/firebase/profile.js
+++ public/js/firebase/profile.js
@@ -15,3 +15,16 @@ async function showGraph() {
         <canvas id="userEventChart" style="width:100%;max-width:100%;"></canvas>
     </div>`
 }
+
+async function updateGraphGuia() {
+    showGraphGuia();
+    makeGraphEventsPerMonth("guiaEventBarChart");
+}
+
+async function showGraphGuia() {
+    const div = document.getElementById("graph");
+    div.innerHTML = `
+    <div id="graphIn">
+        <canvas id="guiaEventBarChart" style="width:100%;max-width:100%;"></canvas>
+    </div>`
+}
